e6fe4f6cab9ad67752f1b6f500e128132f2729d9
Ayush Vaibhav <ayush.vaibhav3@cognizant.com>
2023-12-01 20:32:19 +0530

    Implemented NUnit Assert.AreEqual not working

---
+++ Demo/DemoNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public static void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calculator = new ();
+
+            //Act
+
+            int result = calculator.AddNumbers(10, 20);
+
+            //Assert
+            Assert.Equals(30, result);
+
+        }
+    }
+}
