be6fb01bddb7b43096ccfd3ab7a63610b631d610
Ayush Vaibhav <ayush.vaibhav3@cognizant.com>
2024-01-06 20:22:03 +0530

    String Asserting and Global Initialization

--- Demo/DemoNUnitTest/CalculatorNUnitTests.cs
+++ Demo/DemoNUnitTest/CalculatorNUnitTests.cs
@@ -15,7 +15,7 @@ namespace Demo
         public static void AddNumbers_InputTwoInt_GetCorrectAddition()
         {
             //Arrange
-            Calculator calculator = new ();
+            Calculator calculator = new();
 
             //Act
 
@@ -28,11 +28,55 @@ namespace Demo
 
         [Test]
         public void IsOddCheaker_InputEvenNumber_ReturnFalse()
-        { 
-         Calculator calc = new ();
+        {
+            Calculator calc = new();
             bool isOdd = calc.IsOddNumber(10);
             Assert.That(isOdd, Is.EqualTo(false));
             ClassicAssert.IsFalse(isOdd);                  //Both ways are same
         }
+
+        [Test]
+        [TestCase(11)]
+        [TestCase(13)]
+        public void IsOddCheaker_InputOddNumber_ReturnTrue(int a)
+        {
+            Calculator calc = new();
+            bool isOdd = calc.IsOddNumber(10);
+            Assert.That(isOdd, Is.EqualTo(false));
+        }
+
+        //Combining Multiple Unit Test
+
+        [Test]
+        [TestCase(10,ExpectedResult = false)]
+        [TestCase(13,ExpectedResult = true)]
+
+        public bool IsOddCheaker_InputNumber_RetrunTrueIfOdd(int a)
+        {
+            Calculator calc = new();
+            return calc.IsOddNumber(a);
+        }
+
+        [Test]
+        [TestCase(4.5,10.5)]   //15.9
+        [TestCase(5.43,10.53)]   //15.93
+        [TestCase(5.49,10.59)]   //16.08
+
+        public static void AddNumbers_InputTwoDouble_GetCorrectAddition(double a,double b)
+        {
+            //Arrange
+            Calculator calculator = new();
+
+            //Act
+
+            double result = calculator.AddNumbersDouble(a, b);
+
+            //Assert
+            ClassicAssert.AreEqual(15.9, result,1);          // 1 is used as delta double reasult with range [-1,+1]
+
+        }
+
     }
+
 }
+
---
+++ Demo/DemoNUnitTest/CustomerNUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    [TestFixture]
+    public class CustomerNUnitTest
+    {
+        private Customer customer;     //Global Initilization
+        [SetUp]
+        public void Setup() 
+        { 
+            customer = new Customer();
+        }
+
+        [Test]
+        public void CombineNames_InputFirstAndLastName_ReturnFullName() 
+        { 
+            //Customer customer = new Customer();           //Initialize Globally
+            string fullName = customer.GreetAndCombineNames("Ayush", "Vaibhav");
+            Assert.That(fullName, Is.EqualTo("Hello, Ayush Vaibhav"));
+            
+            ClassicAssert.AreEqual(fullName,("Hello, Ayush Vaibhav"));
+            
+            Assert.That(fullName, Does.Contain("ayush vaibhav").IgnoreCase);
+
+            Assert.That(fullName, Does.StartWith("Hello,"));
+
+            Assert.That(fullName, Does.EndWith("Vaibhav"));
+
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+        }
+        [Test]
+        public void GreetMessage_NotGreated_Return_Null()
+        {
+            //arrange
+            //var customer = new Customer();    //Initialize Globally
+
+            //act
+
+            //Assert
+            ClassicAssert.IsNull(customer.GreetMessage);
+
+        }
+    }
+}
